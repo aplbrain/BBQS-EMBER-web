817d0bd652ab4d7a8e76408da7b35db0c17ae251
Nicole Stock <nicole.stock@jhuapl.edu>
2024-12-16 11:23:20 -0500

    page titles for coming soon pages

--- frontend/src/pages/ToolsPage.vue
+++ frontend/src/pages/ToolsPage.vue
@@ -1,5 +1,12 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    Coming Soon!
+  <q-page class="column items-center">
+    <PageTitle title="Tools" />
+    <div class="q-ma-xl">
+      Coming Soon!
+    </div>
   </q-page>
 </template>
+
+<script setup lang="ts">
+import PageTitle from 'src/components/PageTitle.vue';
+</script>
