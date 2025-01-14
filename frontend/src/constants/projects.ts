ab4d4af069842d339b0bbe41876c3706c81207f8
Nicole Stock <nicole.stock@jhuapl.edu>
2025-01-14 10:39:37 -0500

    Example project. Reformatted metadata to comply with outline on github

--- frontend/src/constants/projects.ts
+++ frontend/src/constants/projects.ts
@@ -1,3 +1,39 @@
 import { ProjectMetadata } from 'src/models/projects';
 
-export const initialProjectList: ProjectMetadata[] = [];
+export const initialProjectList: ProjectMetadata[] = [
+  {
+    funding: {
+      awardTitle: 'Synchronized neuronal and peripheral biomarker recordings in freely moving humans',
+      awardIdentifier: 'R61MH135106',
+      activityCode: 'R61',
+      awardeeOrganization: 'University of California Los Angeles',
+      periodOfPerformance: 0,
+      startDate: new Date('2024-02-21T12:02:00Z'),
+      awardLink: 'https://reporter.nih.gov/project-details/10792386',
+      programOfficer: {
+        name: 'Elizabeth  Ankudowich',
+      },
+    },
+    contributors: [
+      {
+        name: 'Nanthia A. Suthana',
+        email: '',
+        principalInvestigator: true,
+      }
+    ],
+    species: [
+      {
+        taxonomyId: '9606',
+        currentName: 'Homo sapiens',
+        genbankCommonName: 'human',
+        ncbiBlastName: 'primates',
+        rank: 'species',
+        commonName: '',
+      },
+    ],
+    sensors: [],
+    dataModalities: [],
+    approaches: [],
+    dataGenerationSort: [],
+  }
+];
--- frontend/src/models/projects.ts
+++ frontend/src/models/projects.ts
@@ -1,43 +1,76 @@
 import { QTableColumn } from 'quasar';
 
 export interface ProjectMetadata {
-  // TODO!! Metadata Structure
-  grantNumber: string;
-  grantTitle: string;
-  principalInvestigator: string;
+  funding: Funding;
+  contributors: Contributor[];
+  species: Species[];
+  sensors: string[]; // TODO -- what should be the type? should this be broken down?
+  dataModalities: string[]; // TOOD -- what should be the type? should this be broken down?
+  approaches: string[]; // TOOD -- what should be the type? should this be broken down?
+  dataGenerationSort: string[]; // TOOD -- what should be the type? should this be broken down?
+}
+
+export interface Funding {
+  // TODO -- can we auto-populate (some) fields from https://api.reporter.nih.gov/ ?
+  awardTitle: string;
+  awardIdentifier: string;
+  activityCode: string; // i.e. R61, R34, etc.
+  awardeeOrganization: string;
+  periodOfPerformance: number; // (years)
   startDate: Date;
-  modelOrganism: string[];
+  awardLink: string; // Link to NIH RePORTER project details
+  programOfficer: Person;
+}
+
+export interface Person {
+  name: string;
+  email?: string;
+  // TODO -- identifier for person (i.e., ORCID)
+
+}
+
+export interface Contributor extends Person {
+  principalInvestigator: boolean;
+}
+
+export interface Species {
+  taxonomyId: string;
+  currentName: string;
+  genbankCommonName: string;
+  ncbiBlastName: string;
+  rank: string; // TODO Enum ?
+  commonName: string;
 }
 
 export const ProjectTableColumns: QTableColumn[] = [
   {
-    name: 'grantNumber',
+    name: 'awardIdentifier',
     label: 'Grant Number',
-    field: row => row.grantNumber,
+    field: row => row.funding.awardIdentifier,
     required: true,
   },
   {
-    name: 'grantTitle',
+    name: 'awardTitle',
     label: 'Grant Title',
-    field: row => row.grantTitle,
+    field: row => row.funding.awardTitle,
     required: true,
   },
   {
     name: 'principalInvestigator',
     label: 'Principal Investigator',
-    field: row => row.principalInvestigator,
+    field: row => row.contributors.filter((c: Contributor) => c.principalInvestigator).map((c: Contributor) => c.name).join(','),
     required: true,
   },
   {
     name: 'startDate',
     label: 'Project Start Date',
-    field: row => row.startDate,
+    field: row => row.funding.startDate.toISOString(),
     required: true,
   },
   {
-    name: 'modelOrganism',
+    name: 'currentName',
     label: 'Model Orgnaism(s)',
-    field: row => row.modelOrganism,
+    field: row => row.species.map((s: Species) => s.currentName).join(','),
     required: true,
   },
 ]
