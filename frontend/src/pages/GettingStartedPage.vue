56c344ae36ea5296cd54fbda13379b4fa8d46bfc
Nicole Stock <nicole.stock@jhuapl.edu>
2025-01-29 16:21:59 -0500

    getting start page updates

--- frontend/src/pages/GettingStartedPage.vue
+++ frontend/src/pages/GettingStartedPage.vue
@@ -7,7 +7,13 @@
           <p>
             The EMBER Archive supports free storage and sharing of public data, prioritizing data
             generated as a part of the
-            <LinkText :link="links.bbqs_full" />.
+            <LinkText :link="links.bbqs_full" />. Users can create an account to begin instantiating a project and uploading data. Users can also request access to data. As the program progresses, project data will be released freely and publicly following appropriate protocols.
+          </p>
+          <p>
+            <span class="text-bold">
+              Currently, PHI/PII data cannot be submitted to the EMBER Archive.
+            </span>
+            As the BBQS program progresses, EMBER will be adding functionality to support PHI/PII data storage.
           </p>
         </q-card-section>
       </q-card>
@@ -16,7 +22,34 @@
         <q-card-section>
           <div class="text-h2 scrollable" ref="complianceSection">Compliance</div>
 
-          <p></p>
+          <q-list class="row">
+            <q-item class="col-4">
+              <q-item-section>
+                <q-item-label overline>Phase I</q-item-label>
+                <q-item-label>
+                  Users will be required to verify human data have been de-identified and are approved for release and reuse. PHI/PII data will not be accepted.
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item class="col-4">
+              <q-item-section>
+                <q-item-label overline>Phase II</q-item-label>
+                <q-item-label>
+                  Data access controls and data use agreements will be implemented, as needed, in accordance with IRB requirements.
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item class="col-4">
+              <q-item-section>
+                <q-item-label overline>Phase III</q-item-label>
+                <q-item-label>
+                  Additional support for the storage and access control for PHI and PII data will be added.
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
         </q-card-section>
       </q-card>
 
@@ -24,10 +57,47 @@
         <q-card-section>
           <div class="text-h2 scrollable" ref="dataUploadSection">Data Upload</div>
 
-          <p></p>
+          <q-list>
+            <q-item>
+              <q-item-section>
+                <q-item-label overline>Metadata</q-item-label>
+                <q-item-label>
+                  Users can create an EMBER Project, specifying required and optional data for EMBERsets.
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section>
+                <q-item-label overline>Animal Electrophysiology and Behavioral Data</q-item-label>
+                <q-item-label>
+                  Users can upload animal electrophysiology and behavioral data, specified in NWB and BIDS formats, along with supporting data files. This leverages our EMBER-DANDI instance.
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section>
+                <q-item-label overline>De-identified Human Data</q-item-label>
+                <q-item-label>
+                  Appropriately de-identified human data, such as electrophysiology, can be uploaded by users once assuring the appropriate IRB processes are followed and consent documented. Please contact us for more information!
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section>
+                <q-item-label overline>Future Phases</q-item-label>
+                <q-item-label>
+                  Future phases will allow upload of human data requiring access controls or data use agreements, as well as secure upload of PHI/PII information.
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+          </q-list>
         </q-card-section>
 
-        <q-card-section>
+        <!-- <q-card-section>
           <div class="row justify-between">
             <div class="col-12 col-md-4 column flex-center">
               <div class="text-h4">EMBER-DANDI</div>
@@ -47,20 +117,18 @@
               <q-btn label="EMBER-PHI/PII - coming soon"></q-btn>
             </div>
           </div>
-        </q-card-section>
+        </q-card-section> -->
       </q-card>
 
       <q-card flat class="q-mt-lg">
         <q-card-section>
-          <div class="text-h2 scrollable" ref="dataAccessSection">Data Access</div>
-          <p></p>
-        </q-card-section>
-      </q-card>
+          <div class="text-h2 scrollable" ref="dataAccessSection">Data Access & Requests</div>
 
-      <q-card flat class="q-mt-lg">
-        <q-card-section>
-          <div class="text-h2 scrollable" ref="accessRequestSection">Access Request</div>
-          <p></p>
+          <p class="q-px-md q-py-sm">
+            Users can create an account to request data access. As data are released publicly in support of the BBQS program, they will be listed on the
+            <RouterLink to="/projects" class="link">Projects</RouterLink>
+            page.
+          </p>
         </q-card-section>
       </q-card>
 
@@ -85,7 +153,6 @@ const route = useRoute();
 const complianceSection = ref<HTMLElement | null>(null);
 const dataUploadSection = ref<HTMLElement | null>(null);
 const dataAccessSection = ref<HTMLElement | null>(null);
-const accessRequestSection = ref<HTMLElement | null>(null);
 
 onMounted(() => {
   // setTimeout is used to fix a bug where scrolling doesn't work immediately on page load
@@ -101,11 +168,14 @@ onMounted(() => {
         case '#data-access':
           scrollTo(dataAccessSection);
           break;
-        case '#access-request':
-          scrollTo(accessRequestSection);
-          break;
       }
     }
   });
 });
 </script>
+
+<style scoped>
+.q-item__section > .text-overline {
+  font-size: 1rem;
+}
+</style>
--- frontend/src/pages/ProjectsPage.vue
+++ frontend/src/pages/ProjectsPage.vue
@@ -2,33 +2,37 @@
   <q-page>
     <PageTitle title="Projects" />
     <div class="q-ma-xl row">
-      <p>
-        The NIH
-        <LinkText :link="links.brain_initiative" />
-        , through its ongoing
-        <LinkText :link="links.bbqs_full" />
-        , has funded {{ projectCounts.total }} research projects, with the earliest project start
-        date of {{ earliestProjectStart }}. {{ projectCounts.r61r33 }} of the projects are funded
-        through the
-        <LinkText
-          text="R61/R33 mechanism"
-          uri="https://grants.nih.gov/grants/guide/rfa-files/RFA-MH-22-240.html"
-        />
-        intended to support the development and validation of next-generation tools, methods, and
-        analytic approaches to precisely quantify behaviors and combine them with simultaneous
-        recordings of brain activity in humans; {{ projectCounts.r34 }} of the projects are funded
-        through the
-        <LinkText
-          text="R34 mechanism"
-          uri="https://grants.nih.gov/grants/guide/rfa-files/RFA-DA-23-030.html"
-        />
-        , intended to support planning and development of the research framework, design, and
-        approach, including activities that will establish feasibility, validity, and/or other
-        technically qualifying results that, if successful, would support a competitive application
-        for a U01, R01 or equivalent NIH research award.
-      </p>
+      <q-card flat class="q-mt-lg">
+        <q-card-section>
+          <p>
+            The NIH
+            <LinkText :link="links.brain_initiative" />
+            , through its ongoing
+            <LinkText :link="links.bbqs_full" />
+            , has funded {{ projectCounts.total }} research projects, with the earliest project start
+            date of {{ earliestProjectStart }}. {{ projectCounts.r61r33 }} of the projects are funded
+            through the
+            <LinkText
+              text="R61/R33 mechanism"
+              uri="https://grants.nih.gov/grants/guide/rfa-files/RFA-MH-22-240.html"
+            />
+            intended to support the development and validation of next-generation tools, methods, and
+            analytic approaches to precisely quantify behaviors and combine them with simultaneous
+            recordings of brain activity in humans; {{ projectCounts.r34 }} of the projects are funded
+            through the
+            <LinkText
+              text="R34 mechanism"
+              uri="https://grants.nih.gov/grants/guide/rfa-files/RFA-DA-23-030.html"
+            />
+            , intended to support planning and development of the research framework, design, and
+            approach, including activities that will establish feasibility, validity, and/or other
+            technically qualifying results that, if successful, would support a competitive application
+            for a U01, R01 or equivalent NIH research award.
+          </p>
 
-      <p>A quick overview of the BBQS research projects is provided below.</p>
+          <p>A quick overview of the BBQS research projects is provided below.</p>
+        </q-card-section>
+      </q-card>
     </div>
 
     <div class="q-ma-xl row justify-center">
@@ -36,7 +40,7 @@
         :rows="initialProjectList"
         :columns="ProjectTableColumns"
         :rows-per-page-options="[0]"
-        class="col-11"
+        class="col-12"
       >
         <template v-slot:body-cell-awardIdentifier="props">
           <q-td>
