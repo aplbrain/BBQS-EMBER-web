74a62bb739262bd73c074632aab77115f5f569b2
Nicole Tregoning <nicole.tregoning@jhuapl.edu>
2026-01-07 12:12:09 -0500

    Add ruff and backend ci

--- backend/apps/projects/urls.py
+++ backend/apps/projects/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from . import views
 
-
 urlpatterns = [
     path("", views.projects, name="projects"),
     path("<str:project_id>/", views.project, name="project"),
