a7db8043dc62ed4e87c813d55af6c4285ea1f843
Nicole Tregoning <nicole.tregoning@jhuapl.edu>
2026-03-04 14:58:04 -0500

    ci/cd

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -146,8 +146,8 @@ SPECTACULAR_SETTINGS = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:9000", # Native Frontend
-    "http://127.0.0.1:9000", # Native Frontend
-    "http://localhost:8080", # Docker Frontend
-    "https://emberarchive.org", # Production Server
+    "http://localhost:9000",  # Native Frontend
+    "http://127.0.0.1:9000",  # Native Frontend
+    "http://localhost:8080",  # Docker Frontend
+    "https://emberarchive.org",  # Production Server
 ]
